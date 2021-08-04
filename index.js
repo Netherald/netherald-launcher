25fd5da16c624c56a60e548bcfeb8d8077604c2b
myoun <aiden080605@gmail.com>
2021-08-04 20:31:20 +0900

    First Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const ejse = require('ejs-electron')
+const ipcMain = require('electron')
+function createWindow () {
+
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 700,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation : false,
+            nodeIntegration : true
+        }
+    })
+
+    mainWindow.loadFile('index.ejs')
+    mainWindow.maximize()
+    mainWindow.setMenuBarVisibility(false)
+}
+
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function () {
+
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+
+})
+
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
---
+++ main.js
@@ -0,0 +1,54 @@
+function loginclick() {
+    let div = document.getElementById("login-page")
+    div.style.display = "none"
+    div = document.getElementById("main-page")
+    div.style.display = "unset"
+    document.getElementById("html").style.backgroundColor = "#242424"
+    let span = document.getElementById("username")
+    let data = function () {
+        const authserver = "https://authserver.mojang.com/authenticate";
+        var xhr = new XMLHttpRequest()
+        var value = {}
+        xhr.open("POST",authserver,false)
+        xhr.setRequestHeader("Content-Type","application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                let json = JSON.parse(xhr.responseText)
+                console.log(json)
+                value = json;
+            }
+        }
+        let data = JSON.stringify({"agent" : {"name" : "Minecraft", "version" : 1}, "username" : document.getElementById("email").value,"password" : document.getElementById("pw").value})
+        xhr.send(data)
+        return value
+    }()
+    console.log(data['selectedProfile'])
+    span.textContent = data['selectedProfile']['name']
+    let img = document.getElementById("player-head")
+    img.src = "https://cravatar.eu/avatar/"+data['selectedProfile']['name']+".png"
+}
+
+function launch() {
+    const { Client, Authenticator } = require('minecraft-launcher-core');
+    const launcher = new Client();
+    let opts = {
+        clientPackage: null,
+        // For production launchers, I recommend not passing
+        // the getAuth function through the authorization field and instead
+        // handling authentication outside before you initialize
+        // MCLC so you can handle auth based errors and validation!
+        authorization: Authenticator.getAuth(document.getElementById("email").value, document.getElementById("pw").value),
+        root: "./run",
+        version: {
+            number: "1.17.1",
+            type: "release"
+        },
+        memory: {
+            max: "6G",
+            min: "4G"
+        }
+    }
+    launcher.launch(opts);
+    launcher.on('debug', (e) => console.log(e));
+    launcher.on('data', (e) => console.log(e));
+}
